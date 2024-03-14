e87f07d1ec3d4ae313fa986dfcbe0ae8d928c301
palthisowmya-nichebit <sowmya.p@nichebit.com>
2024-03-14 10:33:59 +0530

    IntialCommot

---
+++ src/main/java/com/SpringApplication/SpringProjectDevApplication.java
@@ -0,0 +1,13 @@
+package com.SpringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringApplication/SpringProjectDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
